ee95520f0459b27b22a82d27dfc721e99f830af4
brian <bdkuhn602@gmail.com>
2022-05-31 01:33:10 -0700

    use faker to generate profile pictures

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const { faker } = require('@faker-js/faker');
 
 const App = () => {
-    return <div>Hello, World!</div>;
+    return (
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar" src={faker.image.image()} />
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam
+                    </a>
+                    <div className="metadata">
+                        <span className="date">Today at 6:00 PM</span>
+                    </div>
+                    <div className="text">Nice post.</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
