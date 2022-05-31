6e9c593953eb2192ab6be6a893c718c263999c39
brian <bdkuhn602@gmail.com>
2022-05-31 00:41:22 -0700

    add input with button

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div>
+            <label class="label" for="name">
+                Enter name:
+            </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                Submit
+            </button>
+        </div>
+    );
 };
 
 // Take the react component and show on screen
