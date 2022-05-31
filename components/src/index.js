198499e499dad663c9d2613435beaa9b587ec2d8
brian <bdkuhn602@gmail.com>
2022-05-31 03:24:42 -0700

    fully parameterize comment creation

--- components/src/index.js
+++ components/src/index.js
@@ -2,13 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 
+const { faker } = require('@faker-js/faker');
+
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Jack" />
-            <CommentDetail author="Daniel" />
-            <CommentDetail author="Teal'c" />
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 4:45PM"
+                content="Nice post."
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Jack"
+                timeAgo="Today at 2:00AM"
+                content="Wow!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Daniel"
+                timeAgo="Yesterday at 6:25PM"
+                content="Interesting read."
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Teal'c"
+                timeAgo="Yesterday at 1:27PM"
+                content="Indeed."
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
