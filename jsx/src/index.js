c0d1a3239f7939a285d3108b19e9bdc3373353a3
brian <bdkuhn602@gmail.com>
2022-05-31 00:42:05 -0700

    replace class with className

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
     return (
         <div>
-            <label class="label" for="name">
+            <label className="label" for="name">
                 Enter name:
             </label>
             <input id="name" type="text" />
