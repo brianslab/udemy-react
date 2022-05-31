c3c37000011d7607a4e75d50f279f0d4f6a37435
brian <bdkuhn602@gmail.com>
2022-05-31 01:41:54 -0700

    extract comment JSX to a component

--- components/src/index.js
+++ components/src/index.js
@@ -3,24 +3,7 @@ import ReactDOM from 'react-dom';
 const { faker } = require('@faker-js/faker');
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00 PM</span>
-                    </div>
-                    <div className="text">Nice post.</div>
-                </div>
-            </div>
-        </div>
-    );
+    return <div className="ui container comments" />;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
