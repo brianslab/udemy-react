4d5bb959e4421e1fad9be3e67dc7d670c09c32e0
brian <bdkuhn602@gmail.com>
2022-05-31 00:10:09 -0700

    remove default code

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,7 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Create a react component
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Take the react component and show on screen
