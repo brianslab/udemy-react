ac6f74d77f17c28a9582b47d44d15260c19b03c0
brian <bdkuhn602@gmail.com>
2022-05-31 00:55:09 -0700

    change for to htmlFor

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -8,7 +8,7 @@ const App = () => {
 
     return (
         <div>
-            <label className="label" for="name">
+            <label className="label" htmlFor="name">
                 Enter name:
             </label>
             <input id="name" type="text" />
