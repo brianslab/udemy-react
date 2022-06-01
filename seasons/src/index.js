1c1a6d41178b2bfa76b36f39195e5dc3cfa3f858
brian <bdkuhn602@gmail.com>
2022-06-01 01:01:01 -0700

    update state with current location

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,15 +5,19 @@ class App extends React.Component {
     constructor (props) {
         super(props);
 
+        // the only time a direct assignment to this.state is done is to initialize
         this.state = { lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => console.log(err)
+        );
     }
 
     render () {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-        return <div>Latitude: </div>;
+        return <div>Latitude: {this.state.lat}</div>;
     }
 }
 
