ce3b7c7c966fe82bca42067873267662a9d87585
brian <bdkuhn602@gmail.com>
2022-06-01 01:44:12 -0700

    refactor state initialization to not use constructor

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-    constructor (props) {
-        super(props);
-
-        // the only time a direct assignment to this.state is done is to initialize
-        this.state = { lat: null, errorMessage: '' };
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount () {
         window.navigator.geolocation.getCurrentPosition(
