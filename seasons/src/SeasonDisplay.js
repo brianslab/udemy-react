fc5a18a02db4d7025d68bad1ca5ae75e42088f5b
brian <bdkuhn602@gmail.com>
2022-06-01 01:58:45 -0700

    determine if user is in summer or winter

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
 const SeasonDisplay = (props) => {
-    return <div>Lat: {props.lat}</div>;
+    const season = getSeason(props.lat, new Date().getMonth());
+
+    return <div>Season: {season}</div>;
 };
 
 export default SeasonDisplay;
