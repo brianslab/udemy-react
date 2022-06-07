b3cb275000557918101c688b6a8da8b96ea0f752
brian <bdkuhn602@gmail.com>
2022-06-07 02:45:13 -0700

    create boilerplate src folder

--- src_tpl/index.js
+++ src_tpl/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = () => {
-    return <div>Hello, World!</div>;
-};
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
