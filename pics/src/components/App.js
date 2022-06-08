e3634ed2b6cc31607d4c28aa79f38b6311545d00
brian <bdkuhn602@gmail.com>
2022-06-08 01:12:01 -0700

    print number of results to screen

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,17 +4,25 @@ import SearchBar from './SearchBar';
 import unsplashKey from './keys';
 
 class App extends React.Component {
-    onSearchSubmit (term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params  : { query: term },
-            headers : { Authorization: `Client-ID ${unsplashKey.access}` }
-        });
-    }
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get(
+            'https://api.unsplash.com/search/photos',
+            {
+                params  : { query: term },
+                headers : { Authorization: `Client-ID ${unsplashKey.access}` }
+            }
+        );
+
+        this.setState({ images: response.data.results });
+    };
 
     render () {
         return (
             <div className="ui container" style={{ maginTop: '10px' }}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images.
             </div>
         );
     }
