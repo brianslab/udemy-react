bf7b11c8b9f2f03fa48d63eb4ddbb81565e85f3c
brian <bdkuhn602@gmail.com>
2022-06-09 00:59:53 -0700

    create items that will be shown in the accordion

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Accordion from './Accordion';
 
+const items = [
+    {
+        title   : 'What is React?',
+        content : 'React is a front-end JavaScript framework.'
+    },
+    {
+        title   : 'Why use React?',
+        content : 'React is a favorite JS library among engineers.'
+    },
+    {
+        title   : 'How do you use React?',
+        content : 'You use React by creating components.'
+    }
+];
+
 const App = () => {
     return (
         <div>
-            <Accordion />
+            <Accordion items={items} />
         </div>
     );
 };
