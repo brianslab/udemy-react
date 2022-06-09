dfa6517466277496b5b21a0bf6d8c9514801fce3
brian <bdkuhn602@gmail.com>
2022-06-09 01:28:13 -0700

    configure state for accordion

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map((item) => {
+    const [ activeIndex, setActiveIndex ] = useState(null);
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index);
+    };
+
+    const renderedItems = items.map((item, index) => {
         return (
-            <div key={item.title}>
-                <div className="title active">
+            <React.Fragment key={item.title}>
+                <div
+                    className="title active"
+                    onClick={() => {
+                        onTitleClick(index);
+                    }}
+                >
                     <i className="dropdown icon" />
                     {item.title}
                 </div>
                 <div className="content active">
                     <p>{item.content}</p>
                 </div>
-            </div>
+            </React.Fragment>
         );
     });
 
-    return <div className="ui styled accordion">{renderedItems}</div>;
+    return (
+        <div className="ui styled accordion">
+            {renderedItems}
+            <h1>{activeIndex}</h1>
+        </div>
+    );
 };
 
 export default Accordion;
