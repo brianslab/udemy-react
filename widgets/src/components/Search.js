5d3814b116fffe7d6b8403f4691bec11b9e9bfbf
brian <bdkuhn602@gmail.com>
2022-06-09 04:03:57 -0700

    store search term from user input

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
+    const [ term, setTerm ] = useState('');
+
     return (
         <div>
             <div className="ui form">
                 <div className="field">
                     <label>Enter Search Term</label>
-                    <input className="input" />
+                    <input
+                        className="input"
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                    />
                 </div>
             </div>
         </div>
