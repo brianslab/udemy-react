1458f318b62f79c47b46124f0cb0c3a8852c431b
brian <bdkuhn602@gmail.com>
2022-06-09 03:23:05 -0700

    properly display accordian items

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -8,10 +8,12 @@ const Accordion = ({ items }) => {
     };
 
     const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+
         return (
             <React.Fragment key={item.title}>
                 <div
-                    className="title active"
+                    className={`title ${active}`}
                     onClick={() => {
                         onTitleClick(index);
                     }}
@@ -19,19 +21,14 @@ const Accordion = ({ items }) => {
                     <i className="dropdown icon" />
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
         );
     });
 
-    return (
-        <div className="ui styled accordion">
-            {renderedItems}
-            <h1>{activeIndex}</h1>
-        </div>
-    );
+    return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
