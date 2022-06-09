65cee3fe3730417a840cf0b4588e4401a059ea40
brian <bdkuhn602@gmail.com>
2022-06-09 03:59:29 -0700

    basic text input

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Search = () => {
-    return <h1>Search</h1>;
+    return (
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input className="input" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Search;
