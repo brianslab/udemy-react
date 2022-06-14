3b2d40bf584b5720d08856b185b92efe0c039e69
brian <bdkuhn602@gmail.com>
2022-06-14 01:49:10 -0700

    query wikipedia and return results

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,11 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-    const [ term, setTerm ] = useState('');
+    const [ term, setTerm ] = useState('programming');
+    const [ results, setResults ] = useState([]);
+
+    console.log(results);
 
     useEffect(
         () => {
-            console.log('useEffect has been executed');
+            const search = async () => {
+                const {
+                    data
+                } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                    params : {
+                        action   : 'query',
+                        list     : 'search',
+                        origin   : '*',
+                        format   : 'json',
+                        srsearch : term
+                    }
+                });
+
+                setResults(data.query.search);
+            };
+
+            search();
         },
         [ term ]
     );
