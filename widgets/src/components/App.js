a106802cdd61dcba2c487ab4ad31d6cc8266fc40
brian <bdkuhn602@gmail.com>
2022-06-15 23:20:57 -0700

    start Dropdown component

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './Accordion';
 import Search from './Search';
+import Dropdown from './Dropdown';
 
 const items = [
     {
@@ -20,7 +21,7 @@ const items = [
 const App = () => {
     return (
         <div>
-            <Search />
+            <Dropdown />
         </div>
     );
 };
