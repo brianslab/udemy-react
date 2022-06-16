c0d5efd5b53a62597edc56a963abc33ac9c4b1be
brian <bdkuhn602@gmail.com>
2022-06-15 23:55:25 -0700

    start with dropdown closed and close with each selection

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [ open, setOpen ] = useState(false);
+
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
             return null;
@@ -21,10 +23,15 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <div
+                    className={`ui selection dropdown ${open
+                        ? 'visible active'
+                        : ''}`}
+                    onClick={() => setOpen(!open)}
+                >
                     <i className="dropdown icon" />
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
