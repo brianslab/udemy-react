802ade91fb99e359f3bb3e28cc159bb34eea70cb
brian <bdkuhn602@gmail.com>
2022-06-20 23:05:09 -0700

    add toggle to show dropdown and clean up event listener

--- widgets/src/components/App.js
+++ widgets/src/components/App.js
@@ -26,14 +26,20 @@ const options = [
 
 const App = () => {
     const [ selected, setSelected ] = useState(options[0]);
+    const [ showDropdown, setShowDropdown ] = useState(true);
 
     return (
         <div>
-            <Dropdown
-                selected={selected}
-                onSelectedChange={setSelected}
-                options={options}
-            />
+            <button onClick={() => setShowDropdown(!showDropdown)}>
+                Toggle Dropdown
+            </button>
+            {showDropdown ? (
+                <Dropdown
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                    options={options}
+                />
+            ) : null}
         </div>
     );
 };
