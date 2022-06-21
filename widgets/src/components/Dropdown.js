7363e3035c8728735e699705e6b9fc7ea98ebf7a
brian <bdkuhn602@gmail.com>
2022-06-20 22:41:45 -0700

    add event listener to close dropdown when clicking on body

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,7 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [ open, setOpen ] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener(
+            'click',
+            () => {
+                setOpen(false);
+            },
+            { capture: true }
+        );
+    }, []);
 
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
