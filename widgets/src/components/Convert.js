fc16771ce62c7c55d39a057e878d4236b18bbe03
brian <bdkuhn602@gmail.com>
2022-06-22 23:43:14 -0700

    get translation from google api

---
+++ widgets/src/components/Convert.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import keys from '../api/keys';
+
+const Convert = ({ language, text }) => {
+    const [ translated, setTranslated ] = useState('');
+
+    useEffect(
+        () => {
+            const doTranslation = async () => {
+                const { data } = await axios.post(
+                    'https://translation.googleapis.com/language/translate/v2',
+                    {},
+                    {
+                        params : {
+                            q      : text,
+                            target : language.value,
+                            key    : keys.googleTranslate
+                        }
+                    }
+                );
+
+                setTranslated(data.data.translations[0].translatedText);
+            };
+
+            doTranslation();
+        },
+        [ language, text ]
+    );
+
+    return (
+        <div>
+            <h1 className="ui header">{translated}</h1>
+        </div>
+    );
+};
+
+export default Convert;
--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     { label: 'Afrikaans', value: 'af' },
@@ -28,6 +29,9 @@ const Translate = () => {
                 onSelectedChange={setLanguage}
                 options={options}
             />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     );
 };
