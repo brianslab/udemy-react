5136a1e301b10576d3d7261a92c8e4210a6daa4d
brian <bdkuhn602@gmail.com>
2022-06-29 04:30:47 -0700

    render song data on selection

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = (props) => {
-  return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a Song</div>;
+  }
+
+  return (
+    <div>
+      <h3>Details for:</h3>{' '}
+      <p>
+        Title: {song.title} <br /> Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
