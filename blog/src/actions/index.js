46e4b2c5ac7ab6ed27de73fb9d660fccdb9a228e
brian <bdkuhn602@gmail.com>
2022-06-30 03:02:58 -0700

    get list of posts

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,6 +4,6 @@ export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
   };
 };
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -9,8 +9,13 @@ class PostList extends React.Component {
   }
 
   render () {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
