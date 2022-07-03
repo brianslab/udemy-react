a9d75a42907907e3c5fbf651fd5ca977d3bb3ca5
brian <bdkuhn602@gmail.com>
2022-07-02 23:24:45 -0700

    memoize getUser to prevent duplicate api calls

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => {
@@ -8,10 +10,9 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => {
-  return async (dispatch) => {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({ type: 'FETCH_USER', payload: response.data });
-  };
-};
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+});
