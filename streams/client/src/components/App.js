c5002c80de1316227920be0e1d03fba49d18a507
brian <bdkuhn602@gmail.com>
2022-07-03 03:39:29 -0700

    create basic Header

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './Header';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
@@ -9,9 +10,10 @@ import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
