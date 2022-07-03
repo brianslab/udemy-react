650046bf13c75890b9e2e4a8d4afd51baacf3232
brian <bdkuhn602@gmail.com>
2022-07-02 23:29:46 -0700

    un-memoize fetchUser

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -10,9 +10,16 @@ export const fetchPosts = () => {
   };
 };
 
-export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async (dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+// memoized version:
+// export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
