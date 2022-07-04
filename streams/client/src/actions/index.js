88084770cec197d2004e508eea5593fbdf5f56a7
brian <bdkuhn602@gmail.com>
2022-07-04 00:24:32 -0700

    store user id

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,8 +1,9 @@
 import { SIGN_IN, SIGN_OUT } from './types';
 
-export const signIn = () => {
+export const signIn = (userID) => {
   return {
-    type : SIGN_IN
+    type    : SIGN_IN,
+    payload : userID
   };
 };
 
--- streams/client/src/reducers/authReducer.js
+++ streams/client/src/reducers/authReducer.js
@@ -1,15 +1,16 @@
 import { SIGN_IN, SIGN_OUT } from '../actions/types';
 
 const INITIAL_STATE = {
-  isSignedIn : null
+  isSignedIn : null,
+  userID     : null
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_IN:
-      return { ...state, isSignedIn: true };
+      return { ...state, isSignedIn: true, userID: action.payload };
     case SIGN_OUT:
-      return { ...state, isSignedIn: false };
+      return { ...state, isSignedIn: false, userID: null };
     default:
       return state;
   }
