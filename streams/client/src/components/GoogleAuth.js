62e06e33953550c2862d5feff264d1ebc8bf56e1
brian <bdkuhn602@gmail.com>
2022-07-03 22:51:43 -0700

    begin google auth component

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  componentDidMount () {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId :
+          '739685297559-fqj9fabm4rv57p07c29cgcc3ha2s94kl.apps.googleusercontent.com',
+        scope    : 'email'
+      });
+    });
+  }
+
+  render () {
+    return <div>GoogleAuth</div>;
+  }
+}
+
+export default GoogleAuth;
