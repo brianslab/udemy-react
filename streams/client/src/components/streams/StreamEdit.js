cdfb73a872ed66dc3f5d52c6a3bbe211b6527574
brian <bdkuhn602@gmail.com>
2022-07-05 23:35:09 -0700

    only update title and description

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import { fetchStream, editStream } from '../../actions';
 import StreamForm from './StreamForm';
@@ -22,7 +23,7 @@ class StreamEdit extends React.Component {
       <div>
         <h3>Edit a Stream</h3>
         <StreamForm
-          initialValues={this.props.stream}
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
           onSubmit={this.onSubmit}
         />
       </div>
