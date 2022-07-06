3c8b6bd59977976d98a37383d71167d7734d369e
brian <bdkuhn602@gmail.com>
2022-07-06 01:09:21 -0700

    modal styling

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 const Modal = (props) => {
   return ReactDOM.createPortal(
     <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">Modal</div>
+      <div className="ui standard modal visible active">
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          {' '}
+          Are you sure you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
+      </div>
     </div>,
     document.querySelector('#modal')
   );
