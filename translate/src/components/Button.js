7fafa83da0cd1e587d5fd13ff476925a4c309df7
brian <bdkuhn602@gmail.com>
2022-07-07 00:17:47 -0700

    refactor

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -4,16 +4,20 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
+  renderButton (color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => (value === 'english' ? 'Submit' : 'Voorleggen')}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render () {
     return (
       <ColorContext.Consumer>
-        {(color) => (
-          <button className={`ui button ${color}`}>
-            <LanguageContext.Consumer>
-              {(value) => (value === 'english' ? 'Submit' : 'Voorleggen')}
-            </LanguageContext.Consumer>
-          </button>
-        )}
+        {(color) => this.renderButton(color)}
       </ColorContext.Consumer>
     );
   }
