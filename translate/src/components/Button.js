4e151de51b50d4462e452bb1ea8803921bf3644c
brian <bdkuhn602@gmail.com>
2022-07-06 23:42:19 -0700

    implement context in Field

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -6,9 +6,9 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render () {
-    console.log(this.context);
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
 
-    return <button className="ui button primary">Submit</button>;
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
