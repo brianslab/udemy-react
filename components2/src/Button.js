94c6dd12fe35a7b0790cf4df1c4f40bfcfbf1d2f
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-07 00:13:11 -0800

    prop type checking

--- components2/src/Button.js
+++ components2/src/Button.js
@@ -1,5 +1,33 @@
-function Button({ children }) {
+import PropTypes from 'prop-types';
+
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
   return <button>{children}</button>;
 }
+
+Button.propTypes = {
+  checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger);
+
+    if (count > 1) {
+      return new Error(
+        'Button - only one of primary, secondary, success, warning, danger can be true.'
+      );
+    }
+  },
+};
 
 export default Button;
