ddbe9b3c707d458434065956bb5350db6ceca873
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-09 01:16:48 -0800

    hook up search

--- pics2/src/App.js
+++ pics2/src/App.js
@@ -1,5 +1,15 @@
+import SearchBar from './components/SearchBar';
+
 function App() {
-  return <div>App</div>;
+  const handleSubmit = (term) => {
+    console.log('Do a search with', term);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
--- pics2/src/components/SearchBar.js
+++ pics2/src/components/SearchBar.js
@@ -1,5 +1,25 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+import { useState } from 'react';
+
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState('');
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
