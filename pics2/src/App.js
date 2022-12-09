83a928c08efcd12be2a86d0ea0e181477873875f
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-09 01:57:49 -0800

    perfom api request with user entered term

--- pics2/src/App.js
+++ pics2/src/App.js
@@ -1,8 +1,9 @@
 import SearchBar from './components/SearchBar';
+import searchImages from './api';
 
 function App() {
   const handleSubmit = (term) => {
-    console.log('Do a search with', term);
+    searchImages(term);
   };
 
   return (
