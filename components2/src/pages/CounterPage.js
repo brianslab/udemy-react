0e73092bfa608e9b49d4875cb86cac08183c8d9b
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-29 01:24:50 -0800

    extract usecounter hook

--- components2/src/pages/CounterPage.js
+++ components2/src/pages/CounterPage.js
@@ -1,21 +1,13 @@
-import { useState, useEffect } from 'react';
 import Button from '../components/Button';
+import useCounter from '../hooks/use-counter';
 
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-
-  useEffect(() => {
-    console.log(count);
-  }, [count]);
-
-  const handleClick = () => {
-    setCount(count + 1);
-  };
+  const { count, increment } = useCounter(initialCount);
 
   return (
     <div>
       <h1>Count is {count}</h1>
-      <Button onClick={handleClick}>Increment</Button>
+      <Button onClick={increment}>Increment</Button>
     </div>
   );
 }
