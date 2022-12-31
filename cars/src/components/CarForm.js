47ec4ff3804e930b65131ac8a05899dbf1be79a5
Brian Kuhn <bdkuhn602@gmail.com>
2022-12-31 03:49:47 -0800

    use form to update cars slice

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -1,5 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName } from '../store';
+
 function CarForm() {
-  return <div>CarForm</div>;
+  const dispatch = useDispatch();
+  const name = useSelector((state) => {
+    return state.form.name;
+  });
+
+  const handleNameChange = (event) => {
+    dispatch(changeName(event.target.value));
+  };
+
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Name</label>
+            <input
+              className="input is-expanded"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default CarForm;
