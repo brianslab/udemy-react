c22700fa1f5d7094bf609dc2f3ae570b57ea3f85
brianslab <bdkuhn602@gmail.com>
2023-01-01 23:19:58 -0800

    dispatch during form submission

--- cars/src/components/CarForm.js
+++ cars/src/components/CarForm.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { changeName, changeCost } from '../store';
+import { changeName, changeCost, addCar } from '../store';
 
 function CarForm() {
   const dispatch = useDispatch();
@@ -17,11 +17,16 @@ function CarForm() {
     const carCost = parseInt(event.target.value) || 0;
     dispatch(changeCost(carCost));
   };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(addCar({ name, cost }));
+  };
 
   return (
     <div className="car-form panel">
       <h4 className="subtitle is-3">Add Car</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="field-group">
           <div className="field">
             <label className="label">Name</label>
@@ -41,6 +46,9 @@ function CarForm() {
               type="number"
             />
           </div>
+        </div>
+        <div className="field">
+          <button className="button is-link">Submit</button>
         </div>
       </form>
     </div>
