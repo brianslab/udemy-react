9ebb6e548690e4aba9acb38b70ea137eb7404f60
brianslab <bdkuhn602@gmail.com>
2023-01-02 00:21:47 -0800

    reset form on submit

--- cars/src/store/slices/formSlice.js
+++ cars/src/store/slices/formSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { addCar } from './carsSlice';
 
 const formSlice = createSlice({
   name: 'form',
@@ -13,6 +14,12 @@ const formSlice = createSlice({
     changeCost(state, action) {
       state.cost = action.payload;
     },
+  },
+  extraReducers(builder) {
+    builder.addCase(addCar, (state, action) => {
+      state.name = '';
+      state.cost = 0;
+    });
   },
 });
 
