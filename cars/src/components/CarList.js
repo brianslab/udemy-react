a49482c2b29144f8ab40aff33c59441eb2ce005f
brianslab <bdkuhn602@gmail.com>
2023-01-02 00:16:52 -0800

    handle deletion and add syling

--- cars/src/components/CarList.js
+++ cars/src/components/CarList.js
@@ -1,5 +1,38 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { removeCar } from '../store';
+
 function CarList() {
-  return <div>CarList</div>;
+  const dispatch = useDispatch();
+  const cars = useSelector((state) => {
+    return state.cars.data;
+  });
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar(car.id));
+  };
+
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
--- cars/src/store/slices/carsSlice.js
+++ cars/src/store/slices/carsSlice.js
@@ -4,7 +4,7 @@ const carsSlice = createSlice({
   name: 'cars',
   initialState: {
     searchTerm: '',
-    cars: [],
+    data: [],
   },
   reducers: {
     changeSearchTerm(state, action) {
@@ -13,7 +13,7 @@ const carsSlice = createSlice({
     addCar(state, action) {
       // Assumption:
       // action.payload === { name: 'ab', cost: 140 }
-      state.cars.push({
+      state.data.push({
         name: action.payload.name,
         cost: action.payload.cost,
         id: nanoid(),
@@ -22,10 +22,10 @@ const carsSlice = createSlice({
     removeCar(state, action) {
       // Assumption:
       // action.payload === the id of the car we want to remove
-      const updated = state.cars.filter((car) => {
+      const updated = state.data.filter((car) => {
         return car.id !== action.payload;
       });
-      state.cars = updated;
+      state.data = updated;
     },
   },
 });
