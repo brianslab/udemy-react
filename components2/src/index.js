cfd5c8bd3f48b0db71144db6037ebfe464cc00ce
brianslab <bdkuhn602@gmail.com>
2023-01-17 05:13:47 -0800

    fix components demo project

--- components2/src/index.js
+++ components2/src/index.js
@@ -1,16 +1,14 @@
-import 'bulma/css/bulma.css';
-import './styles.css';
+import './index.css';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import { NavigationProvider } from './context/navigation';
 
 const el = document.getElementById('root');
-const root = createRoot(el);
+const root = ReactDOM.createRoot(el);
 
 root.render(
-  <Provider store={store}>
+  <NavigationProvider>
     <App />
-  </Provider>
+  </NavigationProvider>
 );
